41f6bace359dfd6ea24546d649ef238306a6a41b
faizalardi <faizalardi2016@gmail.com>
2025-11-23 13:07:12 +0700

    Menambahkan dan memisahkan tag head dan icon web

--- view/users/productCollection.php
+++ view/users/productCollection.php
@@ -102,19 +102,7 @@ $collectionProducts = [
 
 ?>
 
-<!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - Customer</title>
-    <link rel="stylesheet" href="/../../src/output.css">
-    <link rel="icon" href="../assets/img/Nanocomp.png">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-</head>
 
 <body class="w-full bg-no-repeat h-screen [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
     <header class="sticky top-0 z-10">
